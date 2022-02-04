24525d17bfef4b43aaba7e59ef5a029422b135f2
marinahaugen <marina.santos.haugen@dnb.no>
2022-02-04 11:28:46 +0100

    add favicon and possibility to switch image

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,33 @@
-const myName = document.querySelector('h2');
-myName.textContent = 'Hello, Ole!';
+const enterNameButton = document.querySelector('button');
+const welcomeText = document.querySelector('h2');
+
+function setAdminName() {
+  let myName = prompt('Name: ');
+  if (!myName) {
+    setAdminName()
+  } else {
+    localStorage.setItem('name', myName);
+    welcomeText.textContent = "Hello, " + myName + "!";
+  }
+}
+
+enterNameButton.onclick = function() {
+  setAdminName()
+}
+
+const img = document.querySelector('img');
+const arrow = document.querySelector('#arrow');
+
+function changeImage() {
+  const srcName = img.getAttribute('src');
+
+  if (srcName === 'images/old-school-cassettes.jpg') {
+    img.setAttribute('src', 'images/old-school-vinyl.jpg');
+  } else {
+    img.setAttribute('src', 'images/old-school-cassettes.jpg');
+  }
+}
+
+arrow.onclick = function() {
+  changeImage();
+}
